f03d6ce90e62c51f9c1db95c456482710b4a4026
admin <admin@usuario>
2018-04-25 17:56:47 -0300

    Foi criado as principais as classes que servirão como base para o armazenamento no banco de dados. As unicas classes que não foram criada foram: ReconhicementoDeEstudos e de ReconhcimentoDeSaberes.

---
+++ ProjectTest/src/main/java/br/com/ifsc/projecttest/model/Disciplina.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifsc.projecttest.model;
+
+/**
+ *
+ * @author admin
+ */
+public class Disciplina {
+    private int id;
+    private String nome;
+    private String cargaHoraria;
+    private String sigla;
+    private Curso curso;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(String cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
